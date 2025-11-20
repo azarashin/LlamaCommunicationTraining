614f716f1709535a11019ae147025498771d973e
azarashin <azarashin1979@gmail.com>
2025-11-21 00:30:47 +0900

    メッセージ表示の予約タスク化。

--- UnityApplication/LlamaCommunicationTraining/Assets/LlamaCommunicationTraining/Scripts/UI/MessageBox.cs
+++ UnityApplication/LlamaCommunicationTraining/Assets/LlamaCommunicationTraining/Scripts/UI/MessageBox.cs
@@ -1,10 +1,21 @@
+using Codice.Client.BaseCommands.BranchExplorer;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 using TMPro;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace llama_communication_training.ui
 {
+    public class MessageReserve
+    {
+        public string TalkerName;
+        public int NamePlateIndex;
+        public string Message;
+    }
     public class MessageBox : MonoBehaviour
     {
         [SerializeField] 
@@ -23,49 +34,75 @@ namespace llama_communication_training.ui
         float _secondsPerCharacter = 0.05f;
 
         [SerializeField]
+        float _intervalForEachMessage = 2.0f; 
+
+        [SerializeField]
         GameObject _nextIcon; 
 
         private Coroutine _typingCoroutine;
+        private List<MessageReserve> _messageQueue = new List<MessageReserve>();
 
         private void Start()
         {
+            _typingCoroutine = StartCoroutine(TypeText());
+
+
             StartTyping("緒方", 0, "これはテストです。これはテストです。これはテストです。");
+            StartTyping("だれか", 1, "これはこれはこれはこれはこれはこれはこれはこれはこれはこれは");
         }
 
         public void StartTyping(string talkerName, int index, string message)
         {
-            // 前回のコルーチンを停止
-            if (_typingCoroutine != null)
-                StopCoroutine(_typingCoroutine);
-
-            _nameLabel.text = talkerName;
-            if(index >= 0 && index < _namePlateList.Length)
+            _messageQueue.Add(new MessageReserve
             {
-                _namePlate.sprite = _namePlateList[index];
-                _namePlate.gameObject.SetActive(true);
-            }
-            else
-            {
-                _namePlate.gameObject.SetActive(false); 
-            }
-            _nextIcon.SetActive(false); 
-
-            _typingCoroutine = StartCoroutine(TypeText(message));
+                TalkerName = talkerName,
+                NamePlateIndex = index,
+                Message = message
+            });
         }
 
-        private IEnumerator TypeText(string message)
+        private IEnumerator TypeText()
         {
-            _message.text = message;
-            _message.maxVisibleCharacters = 0;
-
-            int totalLength = message.Length;
-
-            for (int i = 0; i <= totalLength; i++)
+            while (true)
             {
-                _message.maxVisibleCharacters = i;
-                yield return new WaitForSeconds(_secondsPerCharacter);
+                if (_messageQueue.Count == 0)
+                {
+                    yield return null;
+                    continue;
+                }
+                MessageReserve nextMessage = _messageQueue[0];
+                _messageQueue.RemoveAt(0);
+
+                string message = nextMessage.Message;
+                _nameLabel.text = nextMessage.TalkerName;
+                int index = nextMessage.NamePlateIndex;
+                if (index >= 0 && index < _namePlateList.Length)
+                {
+                    _namePlate.sprite = _namePlateList[index];
+                    _namePlate.gameObject.SetActive(true);
+                }
+                else
+                {
+                    _namePlate.gameObject.SetActive(false);
+                }
+                _nextIcon.SetActive(false);
+
+
+                _message.text = message;
+                _message.maxVisibleCharacters = 0;
+
+                int totalLength = message.Length;
+
+                for (int i = 0; i <= totalLength; i++)
+                {
+                    _message.maxVisibleCharacters = i;
+                    yield return new WaitForSeconds(_secondsPerCharacter);
+                }
+                _nextIcon.SetActive(true);
+                yield return new WaitForSeconds(_intervalForEachMessage);
+
             }
-            _nextIcon.SetActive(true);
+
         }
     }
 }
